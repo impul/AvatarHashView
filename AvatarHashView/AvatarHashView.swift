1474476f1540c84761fe080b9373f4de64e063e0
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2019-04-02 01:48:28 +0300

    Initial Commit

---
+++ AvatarHashView/AvatarHashView.swift
@@ -0,0 +1,11 @@
+//
+//  AvatarHashView.swift
+//  AvatarHashView
+//
+//  Created by Pavlo Boiko on 4/2/19.
+//  Copyright © 2019 Pavlo Boiko. All rights reserved.
+//
+
+class AvatarHashView {
+
+}
