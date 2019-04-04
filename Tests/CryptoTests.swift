f559363e2a2962bfe92fc26f093a3ad56bf29e58
impul <pavlo.bojkoo@gmail.com>
2019-04-05 02:47:49 +0300

    Add hex tests

--- Tests/CryptoTests.swift
+++ Tests/CryptoTests.swift
@@ -10,9 +10,16 @@ import XCTest
 @testable import AvatarHashView
 
 class CryptoTests: XCTestCase {
+    // https://passwordsgenerator.net/sha256-hash-generator/
     func testSha256() {
         let data = Data("Test".utf8)
-        XCTAssertEqual(data.sha256.description, "532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25")
+        XCTAssertEqual(data.sha256.hex, "532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25")
+    }
+    
+    // http://string-functions.com/hex-string.aspx
+    func testHex() {
+        let data = Data("Test".utf8)
+        XCTAssertEqual(data.hex, "54657374")
     }
 }
 
